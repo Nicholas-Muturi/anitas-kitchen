d6e13a4dad3bbe03f1c035c550b8e9c64f55b48d
Nick Muturi <nick@localhost.localdomain>
2018-07-09 15:28:31 +0300

    Added logic to subscription form: It now responds to a new subscriber

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-  $(".toggle#enable i").click(function(e){
+
+  $(".subscribe-section form").submit(function(event){
+    var email= $("input.email-input").val();
+    alert("Thank you " +email+ " for subscribing :)");
+    event.preventDefault();
+  });
+
+  $(".toggle#enable i").click(function(){
     $(".menu").toggleClass("active");
     $(".toggle#enable").hide();
     $(".toggle#disable").show();
-    $(".menu").attr("inactive");
-    e.preventDefault();
   });
 
-  $(".toggle#disable i").click(function(e){
+  $(".toggle#disable i").click(function(){
     $(".menu").toggleClass("active");
     $(".toggle#enable").show();
     $(".toggle#disable").hide();
-    $(".menu").attr("active");
-    e.preventDefault();
   });
+
 });
